9c3db63c1171c0eaca7b1be56fbcabed80ff7b67
Andrii Vasyliev <sol@hiqdev.com>
2015-08-22 05:57:48 +0000

    * mini layout: + powered by

--- src/views/layouts/mini.php
+++ src/views/layouts/mini.php
@@ -45,6 +45,18 @@ JS
     <?= $content; ?>
 </div>
 <!-- /.login-box -->
+
+<div style="position:fixed;bottom:0;width:100%">
+    <div class="text-center"><small>
+        <? if (Yii::$app->params['poweredByName']) { ?>
+            Powered by <a href="<?= Yii::$app->params['poweredByUrl'] ?>"><?= Yii::$app->params['poweredByName'] ?></a>
+            <? if (Yii::$app->params['poweredByVersion']) { ?>
+                version <?= Yii::$app->params['poweredByVersion'] ?>
+            <? } ?>
+        <? } ?>
+    </small></div>
+</div>
+
 <?php $this->endBody(); ?>
 </body>
 </html>
