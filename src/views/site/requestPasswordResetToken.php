3a080c8a96403213bc6b93e49963c45bee4c021a
Andrii Vasyliev <sol@hiqdev.com>
2015-09-28 09:07:44 +0000

    redone and renamed recovery to requestPasswordResetToken to be yii2 canonical

---
+++ src/views/site/requestPasswordResetToken.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model \frontend\models\PasswordResetRequestForm */
+
+$this->blocks['bodyClass'] = 'login-page';
+$this->title = Yii::t('app', 'Request password reset');
+$this->params['breadcrumbs'][] = $this->title;
+
+?>
+<div class="login-box-body">
+    <p class="login-box-msg">Please fill out your email. A link to reset password will be sent there.</p>
+    <?php $form = ActiveForm::begin(['id' => 'request-password-reset-form']); ?>
+        <div class="form-group has-feedback">
+            <?= $form->field($model, 'email')->textInput(['placeholder' => Yii::t('app', 'Email'), 'class' => 'form-control', 'autofocus' => 'autofocus'])->label(false) ?>
+            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-12">
+                <button type="submit" class="btn btn-primary btn-block btn-flat">
+                    <?= Yii::t('app', 'Send') ?>
+                </button>
+            </div>
+        </div>
+    <?php ActiveForm::end() ?>
+</div>
