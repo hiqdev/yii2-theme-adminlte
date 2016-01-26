41e1949224bc9c51ac1d9c153736294df819c8fb
Andrey Klochok <andreyklochok@gmail.com>
2016-01-26 13:29:10 +0200

    Redesigne error page

--- src/views/site/error.php
+++ src/views/site/error.php
@@ -8,20 +8,22 @@ use yii\helpers\Html;
 /* @var $exception Exception */
 
 $this->title = $name;
+$code = substr($name, strpos($name, '#') + 1, 3);
+$textColorClass = $code < 500 ? 'text-yellow' : 'text-red';
+$alertColorClass = $code < 500 ? 'alert-warning' : 'alert-danger';
 ?>
-<div class="site-error">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+<div class="error-page">
+    <h2 class="headline <?= $textColorClass ?>"> 404</h2>
+    <div class="error-content">
+        <h3><i class="fa fa-warning <?= $textColorClass ?>"></i> <?= Html::encode($this->title) ?></h3>
+        <div class="alert <?= $alertColorClass ?>">
+            <?= nl2br(Html::encode($message)) ?>
+        </div>
+        <p>
+            <?= Yii::t('app', 'The above error occurred while the Web server was processing your request.'); ?><br>
+            <?= Yii::t('app', 'Please contact us if you think this is a server error. Thank you.'); ?>
+        </p>
     </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
+    <!-- /.error-content -->
 </div>
