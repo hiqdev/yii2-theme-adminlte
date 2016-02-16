39688ee4fa2020b5b96677ccc4b8a07f90b05274
Andrii Vasyliev <sol@hiqdev.com>
2016-02-16 16:27:30 +0000

    inited tests and CI

---
+++ tests/_bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * AdminLte Theme for hiqdev/yii2-thememanager
+ *
+ * @link      https://github.com/hiqdev/yii2-theme-adminlte
+ * @package   yii2-theme-adminlte
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ */
+
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/../vendor/autoload.php';
