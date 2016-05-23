888a832880bb983b9b684dc14cba6fad9c90f77e
Andrii Vasyliev <sol@hiqdev.com>
2016-05-23 18:13:09 +0000

    fixed resetPassword page to look like others

--- src/views/site/resetPassword.php
+++ src/views/site/resetPassword.php
@@ -7,22 +7,27 @@ use yii\helpers\Html;
 /* @var $form yii\bootstrap\ActiveForm */
 /* @var $model \frontend\models\ResetPasswordForm */
 
-$this->title = 'Reset password';
+$this->title = Yii::t('app', 'Reset password');
+$this->blocks['bodyClass'] = 'register-page';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-reset-password">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please choose your new password:</p>
+<div class="register-box-body">
+    <p class="login-box-msg"><?= Yii::t('app', 'Please choose your new password') ?>:</p>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'reset-password-form']); ?>
-                <?= $form->field($model, 'password')->passwordInput() ?>
-                <div class="form-group">
-                    <?= Html::submitButton('Save', ['class' => 'btn btn-primary']) ?>
-                </div>
-            <?php ActiveForm::end(); ?>
+    <?php $form = ActiveForm::begin(['id' => 'form-signup']) ?>
+        <div class="form-group has-feedback">
+            <?= $form->field($model, 'password')->passwordInput(['placeholder' => 'Password', 'autofocus' => true, 'class' => 'form-control'])->label(false) ?>
+            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
         </div>
-    </div>
-</div>
+        <div class="form-group has-feedback">
+            <?= $form->field($model, 'password_retype')->passwordInput(['placeholder' => 'Retype password', 'class' => 'form-control'])->label(false) ?>
+            <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+        </div>
+        <div class="row">
+            <div class="col-xs-6">
+                <button type="submit" class="btn btn-primary btn-block btn-flat"><?= $this->title ?></button>
+            </div><!-- /.col -->
+        </div>
+    <?php ActiveForm::end(); ?>
+</div><!-- /.register-box-body -->
