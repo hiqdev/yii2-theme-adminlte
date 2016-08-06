2bbfb44ee0f513cc5f824b26aa598f2ce4af6bc0
Andrii Vasyliev <sol@hiqdev.com>
2016-08-06 23:55:36 +0300

    redone with LanguageMenu widget

--- src/views/layouts/navbar-custom-menu.php
+++ src/views/layouts/navbar-custom-menu.php
@@ -2,30 +2,14 @@
 
 use hipanel\widgets\ReminderTop;
 use hiqdev\yii2\cart\widgets\PanelTopCart;
+use hiqdev\yii2\language\widgets\LanguageMenu;
 use yii\helpers\Html;
 use yii\helpers\Url;
 
 ?>
 
 <ul class="nav navbar-nav">
-    <?php if (Yii::$app->get('languageSwitcher', false) !== null): ?>
-        <li class="dropdown language-menu">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                <?= strtoupper(substr(Yii::$app->language, 0, 2)) ?>
-            </a>
-            <ul class="dropdown-menu">
-                <?php foreach (Yii::$app->get('languageSwitcher')->languages as $code => $language): ?>
-                    <li class="header">
-                        <?= Html::a(
-                            Yii::t('hipanel', $language),
-                            Url::to(['/site/language', 'language' => $code]),
-                            ['class' => mb_stristr(Yii::$app->language, $code) ? 'text-bold' : '']
-                        ) ?>
-                    </li>
-                <?php endforeach ?>
-            </ul>
-        </li>
-    <?php endif ?>
+    <?= LanguageMenu::widget() ?>
     <?= ReminderTop::widget() ?>
     <?= PanelTopCart::widget() ?>
     <li class="dropdown user user-menu">
