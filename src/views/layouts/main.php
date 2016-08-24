b12c4737714edc55120fa51da762e751dbfaa796
Andrii Vasyliev <sol@hiqdev.com>
2016-08-24 08:06:40 +0000

    redone subtitle to original Yii style

--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -82,8 +82,8 @@ JS
         <section class="content-header">
             <h1>
                 <?= $this->title ?>
-                <?php if (isset($this->subtitle)) : ?>
-                    <small><?= $this->subtitle ?></small>
+                <?php if (isset($this->params['subtitle'])) : ?>
+                    <small><?= $this->params['subtitle'] ?></small>
                 <?php endif ?>
             </h1>
             <?= Breadcrumbs::widget([
