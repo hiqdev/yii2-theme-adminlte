dd9a622c7608b90dd5ee9ef21b3adc24213587d0
Andrii Vasyliev <sol@hiqdev.com>
2016-09-01 18:22:21 +0000

    used PoweredBy, CopyrightYears, OrganizationLink widgets

--- src/views/layouts/_footer.php
+++ src/views/layouts/_footer.php
@@ -1,13 +1,4 @@
 <footer class="main-footer">
-    <div class="pull-right hidden-xs">
-        <?php if (Yii::$app->params['poweredByName']) : ?>
-            Powered by <a href="<?= Yii::$app->params['poweredByUrl'] ?>"><?= Yii::$app->params['poweredByName'] ?></a>
-            <?php if (Yii::$app->params['poweredByVersion']) : ?>
-                version <?= Yii::$app->params['poweredByVersion'] ?>
-            <?php endif ?>
-        <?php endif ?>
-    </div>
-    <strong>
-        &copy; <?= Yii::$app->themeManager->widget('CopyrightYears') ?> <?= Yii::$app->themeManager->widget('OrganizationLink') ?>.
-    </strong> All rights reserved.
+    <div class="pull-right hidden-xs"><?= Yii::$app->themeManager->widget('PoweredBy') ?> </div>
+    <strong>&copy; <?= Yii::$app->themeManager->widget('CopyrightYears') ?> <?= Yii::$app->themeManager->widget('OrganizationLink') ?></strong>. All rights reserved.
 </footer>
