efacd6238ac243a4721de8c2f0be2779d3d2651c
tofid <andreyklochok@gmail.com>
2016-09-20 18:37:04 +0300

    Applied menuManager in navbar-custom-menu layout view

--- src/views/layouts/navbar-custom-menu.php
+++ src/views/layouts/navbar-custom-menu.php
@@ -1,25 +1,5 @@
-<?php
-
-use hipanel\widgets\ReminderTop;
-use hiqdev\yii2\cart\widgets\PanelTopCart;
-use hiqdev\yii2\language\widgets\LanguageMenu;
-use yii\helpers\Html;
-use yii\helpers\Url;
-
-?>
-
-<ul class="nav navbar-nav">
-    <?= LanguageMenu::widget() ?>
-    <?= ReminderTop::widget() ?>
-    <?= PanelTopCart::widget() ?>
-    <li class="dropdown user user-menu">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-            <?= $this->render('//layouts/gravatar', ['class' => 'user-image']) ?>
-            <span class="hidden-xs"><?= Yii::$app->user->identity->username ?></span>
-        </a>
-        <?= $this->render('//layouts/user-menu') ?>
-    </li>
-    <li>
-        <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
-    </li>
-</ul>
+<?= Yii::$app->menuManager->navbar->render([
+   'options' => [
+       'class' => 'nav navbar-nav'
+   ]
+]) ?>
