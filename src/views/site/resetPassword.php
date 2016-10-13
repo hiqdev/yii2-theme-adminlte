717ad49d76fb031a219373efc7da499117a9cb6c
Andrii Vasyliev <sol@hiqdev.com>
2016-10-13 14:48:36 +0000

    greatly improved LoginForm to be suitable for any login-like pages: signup, restore-password, reset-password

--- src/views/site/resetPassword.php
+++ src/views/site/resetPassword.php
@@ -1,32 +1,14 @@
 <?php
 
-use yii\bootstrap\ActiveForm;
+/** @var yii\web\View $this */
 
-/* @var $this yii\web\View */
-/* @var $form yii\bootstrap\ActiveForm */
-/* @var $model \frontend\models\ResetPasswordForm */
-
-$this->title = Yii::t('adminlte', 'Reset password');
-$this->blocks['bodyClass'] = 'register-page';
+$this->title = Yii::t('hisite', 'Reset password');
 $this->params['breadcrumbs'][] = $this->title;
+
 ?>
 
-<div class="register-box-body">
-    <p class="login-box-msg"><?= Yii::t('adminlte', 'Please choose your new password') ?>:</p>
-
-    <?php $form = ActiveForm::begin(['id' => 'form-signup']) ?>
-        <div class="form-group has-feedback">
-            <?= $form->field($model, 'password')->passwordInput(['placeholder' => 'Password', 'autofocus' => true, 'class' => 'form-control'])->label(false) ?>
-            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-        </div>
-        <div class="form-group has-feedback">
-            <?= $form->field($model, 'password_retype')->passwordInput(['placeholder' => 'Retype password', 'class' => 'form-control'])->label(false) ?>
-            <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
-        </div>
-        <div class="row">
-            <div class="col-xs-6">
-                <button type="submit" class="btn btn-primary btn-block btn-flat"><?= $this->title ?></button>
-            </div><!-- /.col -->
-        </div>
-    <?php ActiveForm::end(); ?>
-</div><!-- /.register-box-body -->
+<?= Yii::$app->themeManager->widget([
+    'class' => 'LoginForm',
+    'model' => $model,
+    'message' => Yii::t('hisite', 'Please choose your new password'),
+]) ?>
--- src/views/widgets/LoginForm.php
+++ src/views/widgets/LoginForm.php
@@ -6,7 +6,8 @@ use yii\widgets\ActiveForm;
 
 /** @var yii\web\View $this */
 /** @var yii\widgets\ActiveForm $form */
-/** @var LoginForm $model */
+/** @var yii\base\Model $model */
+/** @var yii\base\Widget $widget */
 
 $this->registerCss(<<<'CSS'
     .social-button-login > a:nth-child(n+3){
@@ -19,7 +20,6 @@ CSS
 );
 
 $this->blocks['bodyClass'] = 'login-page';
-$this->title = Yii::t('adminlte', 'Sign in');
 $this->registerCss(<<<'CSS'
     .checkbox label {
         padding-left: 0px;
@@ -29,62 +29,91 @@ CSS
 ?>
 
 <div class="login-box-body">
-    <p class="login-box-msg"><?= Yii::t('adminlte', 'Sign in to start your session') ?></p>
+    <h1><?= Html::encode($this->title) ?></h1>
+    <p class="login-box-msg"><?= $widget->message ?></p>
+
     <?php $form = ActiveForm::begin(['id' => 'login-form']) ?>
-        <div class="form-group has-feedback">
-            <?= $form->field($model, 'username')->textInput([
+        <?php if ($model->isAttributeActive('username')) : ?>
+            <div class="form-group has-feedback">
+                <?= $form->field($model, 'username')->textInput([
                     'placeholder' => Yii::t('adminlte', 'Login or Email'),
                     'class' => 'form-control',
                     'autofocus' => empty($model->username) ? 'autofocus' : false,
-            ])->label(false) ?>
-            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-        </div>
-        <div class="form-group has-feedback">
-            <?= $form->field($model, 'password')->passwordInput([
+                ])->label(false) ?>
+                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+            </div>
+        <?php endif ?>
+        <?php if ($model->isAttributeActive('email')) : ?>
+            <div class="form-group has-feedback">
+                <?= $form->field($model, 'email')->textInput([
+                    'placeholder' => 'Email',
+                    'class' => 'form-control',
+                    'autofocus' => empty($model->email) ? 'autofocus' : false,
+                ])->label(false) ?>
+                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+            </div>
+        <?php endif ?>
+        <?php if ($model->isAttributeActive('password')) : ?>
+            <div class="form-group has-feedback">
+                <?= $form->field($model, 'password')->passwordInput([
                     'placeholder' => Yii::t('adminlte', 'Password'),
                     'class' => 'form-control',
                     'autofocus' => (empty($model->password) && !empty($model->username)) ? 'autofocus' : false,
-            ])->label(false) ?>
-            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-        </div>
-        <div class="row">
-            <div class="col-xs-8">
-                <div class="checkbox icheck">
-                    <?= $form->field($model, 'rememberMe')->checkbox([])->label(false) ?>
-                </div>
+                ])->label(false) ?>
+                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
             </div>
-            <div class="col-xs-4">
-                <button type="submit" class="btn btn-primary btn-block btn-flat"><?= Yii::t('adminlte', 'Sign in') ?></button>
+        <?php endif ?>
+        <?php if ($model->isAttributeActive('password_retype')) : ?>
+            <div class="form-group has-feedback">
+                <?= $form->field($model, 'password_retype')->passwordInput([
+                    'placeholder' => Yii::t('adminlte', 'Retype password'),
+                    'class' => 'form-control'
+                ])->label(false) ?>
+                <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+            </div>
+        <?php endif ?>
+        <div class="row">
+            <?php $submitSize = 12 ?>
+            <?php if ($model->isAttributeActive('rememberMe')) : ?>
+                <div class="col-xs-8">
+                    <div class="checkbox icheck">
+                        <?= $form->field($model, 'rememberMe')->checkbox([])->label(false) ?>
+                    </div>
+                </div>
+                <?php $submitSize = 4 ?>
+            <?php endif ?>
+            <div class="col-xs-<?= $submitSize ?>">
+                <button type="submit" class="btn btn-primary btn-block btn-flat"><?= $this->title ?></button>
             </div>
         </div>
     <?php $form->end() ?>
 
-    <?php $authAuthChoice = AuthChoice::begin([
-        'baseAuthUrl' => ['auth'],
-        'options' => ['class' => 'social-auth-links text-center'],
-    ]) ?>
-        <p>-- <?= Yii::t('adminlte', 'OR SIGN IN WITH') ?> --</p>
-        <div class="social-button-login">
-            <div class="row">
-            <?php foreach ($authAuthChoice->getClients() as $name => $client): ?>
-                <div class="col-md-6 col-xs-12" style="margin-bottom: 0.5em">
-                    <?php $letter = $name==='yandex' ? 'Я' : '' ?>
-                    <?php $class = $name==='live' ? 'windows' : $name ?>
-                    <?php $text = sprintf('<i class="%s">%s</i>&nbsp;%s', "fa fa-$class", $letter, $client->getTitle()) ?>
-                    <?= $authAuthChoice->clientLink($client, $text, ['class' => "btn btn-block btn-social btn-$class"]) ?>
+    <?php if ($widget->isShown('social-login')) : ?>
+        <?php $authAuthChoice = AuthChoice::begin([
+            'baseAuthUrl' => ['auth'],
+            'options' => ['class' => 'social-auth-links text-center'],
+        ]) ?>
+            <p>-- <?= Yii::t('adminlte', 'OR SIGN IN WITH') ?> --</p>
+            <div class="social-button-login">
+                <div class="row">
+                <?php foreach ($authAuthChoice->getClients() as $name => $client): ?>
+                    <div class="col-md-6 col-xs-12" style="margin-bottom: 0.5em">
+                        <?php $letter = $name==='yandex' ? 'Я' : '' ?>
+                        <?php $class = $name==='live' ? 'windows' : $name ?>
+                        <?php $text = sprintf('<i class="%s">%s</i>&nbsp;%s', "fa fa-$class", $letter, $client->getTitle()) ?>
+                        <?= $authAuthChoice->clientLink($client, $text, ['class' => "btn btn-block btn-social btn-$class"]) ?>
+                    </div>
+                <?php endforeach ?>
                 </div>
-            <?php endforeach ?>
             </div>
-        </div>
-    <?php AuthChoice::end() ?>
-
-    <?php if ($restorePasswordPage) : ?>
-        <?= Html::a(Yii::t('adminlte', 'I forgot my password'), $restorePasswordPage) ?>
-        <br/>
+        <?php AuthChoice::end() ?>
     <?php endif ?>
 
-    <?php if ($signupPage) : ?>
-        <?= Html::a(Yii::t('adminlte', 'Register a new membership'), $signupPage) ?>
-    <?php endif ?>
+    <?php foreach (['restore-password', 'signup', 'login'] as $action) : ?>
+        <?php if ($widget->isShown($action)) : ?>
+            <?= Html::a($widget->getText($action), $widget->getPage($action)) ?>
+            <br/>
+        <?php endif ?>
+    <?php endforeach ?>
 
 </div>
