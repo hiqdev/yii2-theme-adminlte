30d60fd0042a15b57040656ab0677673804cc13d
Andrii Vasyliev <sol@hiqdev.com>
2016-10-27 15:40:53 +0000

    fixed miniLanguageMenu view

--- src/views/widgets/miniLanguageMenu.php
+++ src/views/widgets/miniLanguageMenu.php
@@ -7,13 +7,16 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 
 ?>
-<?php
-if (is_array($languages)) : ?>
+<?php if (is_array($languages)) : ?>
     <ul class="list-inline">
-        <?php foreach ($languages as $code => $language) : ?>
-            <?= Html::beginTag('li') ?>
-            <?= Html::a($language, Url::to(array_merge($selectUrl, ['language' => $code])), ['class' => mb_stristr($language, $code) ? 'text-bold' : '']) ?>
-            <?= Html::endTag('li') ?>
+        <?php foreach ($languages as $code => $lang) : ?>
+            <li>
+                <?php if (mb_stristr($language, $code)) : ?>
+                    <?= $lang ?>
+                <?php else : ?>
+                    <?= Html::a($lang, Url::to(array_merge($selectUrl, ['language' => $code]))) ?>
+                <?php endif ?>
+            </li>
         <?php endforeach; ?>
     </ul>
 <?php endif; ?>
