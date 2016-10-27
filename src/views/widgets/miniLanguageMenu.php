aa37a771a5530717648c1ea5e851aad3afe00e54
tofid <andreyklochok@gmail.com>
2016-10-27 19:12:28 +0300

    Added language delimiter to miniLangMenu view

--- src/views/widgets/miniLanguageMenu.php
+++ src/views/widgets/miniLanguageMenu.php
@@ -1,22 +1,27 @@
 <?php
 
 /** @var array $languages */
+/** @var string $language */
 /** @var array|mixed $selectUrl */
 
 use yii\helpers\Html;
 use yii\helpers\Url;
 
+$delimiter = '&nbsp;·&nbsp;';
+$out = '';
 ?>
 <?php if (is_array($languages)) : ?>
     <ul class="list-inline">
-        <?php foreach ($languages as $code => $lang) : ?>
-            <li>
-                <?php if (mb_stristr($language, $code)) : ?>
-                    <?= $lang ?>
-                <?php else : ?>
-                    <?= Html::a($lang, Url::to(array_merge($selectUrl, ['language' => $code]))) ?>
-                <?php endif ?>
-            </li>
-        <?php endforeach; ?>
+        <?php
+        foreach ($languages as $code => $lang) {
+            if (mb_stristr($language, $code)) {
+                $out .= Html::tag('li', $lang);
+            } else {
+                $out .= Html::tag('li', Html::a($lang, Url::to(array_merge($selectUrl, ['language' => $code]))));
+            }
+            $out .= $delimiter;
+        }
+        print rtrim($out, $delimiter);
+        ?>
     </ul>
 <?php endif; ?>
