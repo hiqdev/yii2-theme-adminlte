b44d0576eae1ffb43b8e1e5f802b6bfbc56de7a7
tofid <andreyklochok@gmail.com>
2017-04-13 18:22:04 +0300

    Added view files for Faq widget

---
+++ src/views/widgets/faq/root.php
@@ -0,0 +1,70 @@
+<?php
+
+use yii\helpers\Html;
+
+/** @var array $items */
+/** @var \yii\web\View $this */
+/** @var \hipanel\faq\widgets\Faq $this ->context */
+$this->registerCss("
+#faq .box.box-solid > .box-header a:hover {
+    background: none;
+}
+");
+
+?>
+<div id="faq" class="nav-tabs-custom faq-categories">
+    <ul class="nav nav-tabs">
+        <?php foreach ($items as $tabId => $tab): ?>
+            <li>
+                <?= Html::a(sprintf('%s&nbsp;&nbsp;<span class="label label-primary">%d</span>', $tab['label'], count($tab['items'])), '#tab-' . $tabId, ['data-toggle' => 'tab']) ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+    <div class="tab-content">
+        <?php foreach ($items as $tabId => $tab) : ?>
+            <div class="tab-pane fade" id="<?= 'tab-' . $tabId ?>">
+                <div id="<?= $tabId ?>" class="lpanel-group spacing-40">
+                    <?php foreach ($tab['items'] as $itemId => $item) : ?>
+                        <?= $this->render('node', ['item' => $item, 'itemId' => $itemId, 'parentId' => $tabId]) ?>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</div>
+
+<!--div id="faq" class="faq-tabs">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="faq-categories">
+                <ul>
+                    <?php foreach ($items as $tabId => $tab): ?>
+                        <li>
+                            <?= Html::a(sprintf('%s<span class="badge">%d</span>', $tab['label'], count($tab['items'])), '#tab-' . $tabId, ['data-toggle' => 'tab']) ?>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="faq-tabs-white">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="tabbable tabs-top-horizontal">
+                <div class="tab-content">
+                    <?php foreach ($items as $tabId => $tab) : ?>
+                        <div class="tab-pane fade" id="<?= 'tab-' . $tabId ?>">
+                            <div id="<?= $tabId ?>" class="panel-group spacing-40">
+                                <?php foreach ($tab['items'] as $itemId => $item) : ?>
+                                    <?= $this->render('node', ['item' => $item, 'itemId' => $itemId, 'parentId' => $tabId]) ?>
+                                <?php endforeach; ?>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div-->
