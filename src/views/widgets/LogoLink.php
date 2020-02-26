326c679503a116fa8275c9583f3576fb71712d1b
Andrii Vasyliev <sol@hiqdev.com>
2020-02-26 13:47:33 +0000

    Showing logo text if no image at `LogoLink`

--- src/views/widgets/LogoLink.php
+++ src/views/widgets/LogoLink.php
@@ -4,10 +4,18 @@ use yii\helpers\Html;
 
 ?>
 <?= Html::beginTag('a', array_merge($options, ['href' => $url, 'class' => 'logo'])) ?>
-    <span class="logo-mini">
-        <?= Html::img($smallImage, $smallImageOptions) ?>
-    </span>
-    <span class="logo-lg">
-        <?= Html::img($image, $imageOptions) ?>
-    </span>
+    <?php if (empty($image)) : ?>
+        <span class="logo-text"><?= $name ?></span>
+    <?php else : ?>
+        <?php if ($smallImage) : ?>
+            <span class="logo-mini">
+                <?= Html::img($smallImage, $smallImageOptions) ?>
+            </span>
+        <?php endif ?>
+        <?php if ($image) : ?>
+            <span class="logo-lg">
+                <?= Html::img($image, $imageOptions) ?>
+            </span>
+        <?php endif ?>
+    <?php endif ?>
 <?= Html::endTag('a') ?>
