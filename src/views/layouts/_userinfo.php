b3dd55e74e832814116aa9636229872d0ededb36
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2021-03-26 23:22:23 +0200

    Strict formatting

--- src/views/layouts/_userinfo.php
+++ src/views/layouts/_userinfo.php
@@ -1,4 +1,10 @@
-<p><?= isset(Yii::$app->user->identity->name) ? Yii::$app->user->identity->name : 'User Name' ?></p>
+<?php
+
+use yii\helpers\Html;
+
+?>
+
+<p><?= isset(Yii::$app->user->identity->name) ? Html::encode(Yii::$app->user->identity->name) : 'User Name' ?></p>
 <a href="#">
     <i class="fa fa-circle text-success"></i>Online
 </a>
