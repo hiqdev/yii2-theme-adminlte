ca2b4c330a37370ad7666e62bba5cdd71ccd20d8
SilverFire - Dmytro Naumenko <d.naumenko.a@gmail.com>
2025-06-09 15:14:30 +0300

    Make sure /app/runtime is writable for the application

--- src/views/layouts/_header.php
+++ src/views/layouts/_header.php
@@ -23,6 +23,10 @@ use hiqdev\thememanager\widgets\LogoLink;
     <?= LogoLink::widget() ?>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top" role="navigation">
+        <div class="alert alert-warning" role="alert" style="margin: 0; padding: 4px 10px; border-radius: 0;">
+            <i class="fa fa-exclamation-triangle"></i> <strong>Currently, the company's infrastructure is experiencing a DDoS attack!</strong>
+            <p>You may encounter degraded service performance. We are working to mitigate the attack, and the situation is under control. We apologize for any inconvenience.</p>
+        </div>
         <!-- Sidebar toggle button-->
         <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
             <span class="sr-only">Toggle navigation</span>
